5d592ac041a50649f1ef5a45f368cab9eb6a63a6
jjub0217 <noonssub1004@gmail.com>
2021-01-11 20:09:44 +0900

    Movie component

--- src/movie.js
+++ src/movie.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import './Movie.css';
 
 
-function Movie({title, year, summary, poster}){
+function Movie({title, year, summary, poster, genres}){
     return (
-    <div class="movie">
+    <div className="movie">
         <img src={poster} alt={title} title={title} />
-        <div class="movie__data">
-            <h3 class="movie__title">{title}</h3>
-            <h5 class="movie__year">{year}</h5>
-            <p class="movie__summary">{summary}</p>
+        <div className="movie__data">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className="movie__genres">{genres.map((genre, i)=>{
+                return <li key={i} className="movie__genres">{genre}</li>
+            })}
+            </ul>
+            <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
     </div>
     )  
@@ -22,6 +26,7 @@ Movie.propTypes = {
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 
